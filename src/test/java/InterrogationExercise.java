7986f312b18a11e8206e9c9ce5c8bfe37e34a853
Igo199 <igor19911@gmail.com>
2017-05-15 11:35:13 +0200

    Code refactoring

--- src/test/java/InterrogationExercise.java
+++ src/test/java/InterrogationExercise.java
@@ -1,24 +1,35 @@
-import org.junit.Assert;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class InterrogationExercise {
+
+    public static WebDriver driver;
+
+    @BeforeClass
+    public static void createDriver(){
+        driver = new FirefoxDriver();
+    }
 
     @Test
     public void driverInterrogate(){
 
-        WebDriver driver = new FirefoxDriver();
-
         final String pageURL = "http://compendiumdev.co.uk/selenium/basic_web_page.html";
 
         driver.navigate().to(pageURL);
-        Assert.assertEquals(driver.getTitle(), "Basic Web Page Title");
-        Assert.assertEquals(driver.getCurrentUrl(), pageURL);
+        
+        assertThat(driver.getTitle(), is("Basic Web Page Title"));
+        assertThat(driver.getCurrentUrl(), is(pageURL));
+        assertThat(driver.getPageSource(), containsString("A paragraph of text"));
+    }
 
-        String pageSource = driver.getPageSource();
-        Assert.assertTrue(pageSource.contains("A paragraph of text"));
-
-        System.out.println(pageSource);
+    @AfterClass
+    public static void closeDriver(){
+        driver.quit();
     }
 }
